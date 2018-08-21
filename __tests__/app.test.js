702be9f62b7c7283688144a9c54f5b82db804df5
tektechnologies <info@tektechnologies.com>
2018-08-21 16:38:07 -0500

    adds test for app

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,14 @@
 const request = require('supertest');
 
 const server = require('../src/app');
+const app = require('../src/app');
 
 describe('app', () => {
+  it('responds with 404 for unknown path', () => {
+    return request(app)
+      .get('/404')
+      .expect(404)
+      .expect('Content-Type', 'text/html')
+      .expect('Resource Not Found');
+  });
 });
