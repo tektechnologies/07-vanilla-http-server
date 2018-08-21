fdb371cbceaecc4c09d1e176f6bec8f3eb714397
tektechnologies <info@tektechnologies.com>
2018-08-21 16:51:47 -0500

    add the post test to apptest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,4 +13,23 @@ describe('app', () => {
       .expect('Content-Type', 'text/html')
       .expect('Resource Not Found');
   });
-});
+  it('responds with HTML for /', () => {
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect(response => {expect(response.text[0]).toBe('<');
+      });
+  });
+
+  it('respond with 500 for /500', () => {
+    return request(app)
+      .post('/500')
+      .expect(500)
+      .expect('Content-Type', 'text/html')
+      .expect('Test Error');
+  });
+
+
+
+});//closes describe app
