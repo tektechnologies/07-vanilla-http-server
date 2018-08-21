3710986ae9b882373d4aca79098f4c4afef466ff
tektechnologies <info@tektechnologies.com>
2018-08-21 16:31:15 -0500

    this updates app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,22 @@
 const http = require('http');
 
 const requestParser = require('./lib/request-parser');
+
+
+const app = http.createServer(requestHandler);
+module.exports = app;
+app.start = (port) => 
+  new Promise((resolveCallback, rejectCallback) => {
+    app.listen(port, (err, result) => {
+      if(err) {
+        rejectCallback(err);
+      }else{ 
+        resolveCallback(result);
+      }
+    });
+  });
+
+
+function requestHandler(req,res){
+
+}
